8946888f9ee23670f8badc2212c7499e79ec85de
OmBharti999 <55656898+OmBharti999@users.noreply.github.com>
2022-10-06 21:24:27 +0530

    Update List.jsx

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,8 +5,8 @@ import FlipMove from "react-flip-move";
 export default function List(p) {
   const listItems = p.arr.map((item, i) => (
     <div key={i} className="list">
-      <input value={item} onClick={(e) => p.edit(e.target.value, i)} />
-      <i class="fa-solid fa-trash" onClick={() => p.del(i)}></i>
+      <input value={item} onChange={(e) => p.edit(e.target.value, i)} />
+      <i className="fa-solid fa-trash" onClick={() => p.del(i)}></i>
     </div>
   ));
   return (

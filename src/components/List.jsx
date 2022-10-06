4ae6bfd9d9ee4decfe43c624592374af407a3346
OmBharti999 <55656898+OmBharti999@users.noreply.github.com>
2022-10-06 23:40:27 +0530

    Update List.jsx

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,9 +3,11 @@ import "./list.css";
 import FlipMove from "react-flip-move";
 
 export default function List(p) {
+  const date = new Date();
   const listItems = p.arr.map((item, i) => (
     <div key={i} className="list">
       <input value={item} onChange={(e) => p.edit(e.target.value, i)} />
+      <span>{`${date.getDay()}-${date.getMonth()}-${date.getFullYear()}`}</span>{" "}
       <i className="fa-solid fa-trash" onClick={() => p.del(i)}></i>
     </div>
   ));
